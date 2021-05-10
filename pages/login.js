5caa09524fd4b0cada395ac493eccddb4e936336
OmarMZidan <omarzidan87@outlook.com>
2021-05-11 00:38:06 +0200

    edits

--- pages/login.js
+++ pages/login.js
@@ -7,16 +7,42 @@ const Login = () => {
   const handleSignIn = () => {
     auth.signInWithPopup(provider).catch(alert);
   };
+
+  const handleGuest = () => {
+    auth
+      .signInWithEmailAndPassword("test@test.com", "testtest")
+      .then((userCredential) => {})
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      });
+  };
   return (
     <Container>
       <Head>
-        <title>Login</title>
+        <title>WhatsApp</title>
       </Head>
-
+      <Header>
+        <HeaderLogo
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+          alt="WhatsApp Logo"
+        />
+        <PageTitle>WHATSAPP WEB</PageTitle>
+      </Header>
       <LoginContainer>
-        <Logo src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" />
-        <Button onClick={handleSignIn} variant="outlined">
-          Sign in with Google
+        <Logo
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
+          alt="WhatsApp Logo"
+        />
+        <Button
+          onClick={handleSignIn}
+          variant="outlined"
+          style={{ marginBottom: "20px" }}
+        >
+          Sign in with your Google account
+        </Button>
+        <Button onClick={handleGuest} variant="outlined">
+          Sign in as a Guest
         </Button>
       </LoginContainer>
     </Container>
@@ -29,7 +55,20 @@ const Container = styled.div`
   display: grid;
   place-items: center;
   height: 100vh;
-  background-color: whitesmoke;
+  background: linear-gradient(0deg, #dddbd1 65%, #00bfa5 35%);
+`;
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const PageTitle = styled.span`
+  color: white;
+  margin-left: 10px;
+`;
+const HeaderLogo = styled.img`
+  height: 40px;
+  width: 40px;
 `;
 const LoginContainer = styled.div`
   display: flex;
@@ -37,7 +76,7 @@ const LoginContainer = styled.div`
   padding: 100px;
   align-items: center;
   background-color: white;
-  border-radius: 5px;
+  border-radius: 3px;
   box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
 `;
 const Logo = styled.img`
